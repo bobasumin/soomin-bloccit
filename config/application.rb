0be6c5cbd468e7ed091e5dd23de6a195df408263
Soomin <bobasumin@gmail.com>
2014-01-03 12:10:00 -0500

    Updated application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-config.assets.initialize_on_precompile = false
+
 
 require File.expand_path('../boot', __FILE__)
 
@@ -19,6 +19,7 @@ end
 
 module Bloccit
   class Application < Rails::Application
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
