6c4664be60f804d677b2f7d9b4d7d35243ed4cac
Soomin <bobasumin@gmail.com>
2014-01-15 17:34:29 +0900

    Edited Image Uploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,16 +4,16 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :fog
 
-  process :resize_to_fill => [300, 300]
+  #process :resize_to_fill => [300, 300]
 
-  version :thumb do
-    process :resize_to_fill => [50, 50]
-  end
+  #version :thumb do
+    #process :resize_to_fill => [50, 50]
+  #end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -30,16 +30,16 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process :scale => [200, 300]
+  process :scale => [300, 300]
   #
-  # def scale(width, height)
-  #   # do something
-  # end
+  def scale(width, height)
+     do something
+  end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :scale => [50, 50]
-  # end
+  version :thumb do
+     process :scale => [50, 50]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
